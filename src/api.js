a88823068a639036c5d663d6ace8512623e578ad
cordel22 <cordelfenevall@gmail.com>
2023-03-04 01:23:13 +0100

    figgure out views

--- src/api.js
+++ src/api.js
@@ -4,15 +4,16 @@ const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 //		this doesnt work:(
-app.set("views", __dirname + "/views");
-app.set('view-engine', 'ejs')
+//	app.set("views", __dirname + "/views");
+
+//	app.set('view-engine', 'ejs')
 
 router.get("/", (req, res) => {
-/*
+
   res.json({
     hello: "figgure out views next ma niggaz!"
-  });		this doesnt work:(	*/
-  res.render('index.ejs')
+  });	/*	this doesnt work:(
+  res.render('./views/index.ejs')	*/
 });
 
 app.use(`/.netlify/functions/api`, router);
