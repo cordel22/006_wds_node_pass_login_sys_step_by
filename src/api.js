4a04ee6b84109697d4e3d209926dd1458eb88b03
cordel22 <cordelfenevall@gmail.com>
2023-03-04 01:12:59 +0100

    views setup now

--- src/api.js
+++ src/api.js
@@ -8,11 +8,11 @@ app.set("views", __dirname + "/views");
 app.set('view engine', 'ejs')
 
 router.get("/", (req, res) => {
-
+/*
   res.json({
     hello: "figgure out views next ma niggaz!"
-  });	/*	this doesnt work:(
-  res.render('./views/index.ejs')	*/
+  });		this doesnt work:(	*/
+  res.render('./views/index.ejs')
 });
 
 app.use(`/.netlify/functions/api`, router);
