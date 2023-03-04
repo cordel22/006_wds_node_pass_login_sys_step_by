652f01c1d796079d6d5ab5b667ca7abc2fddb819
cordel22 <cordelfenevall@gmail.com>
2023-03-04 01:08:59 +0100

    views set

--- readme.txt
+++ readme.txt
@@ -1,1 +1,6 @@
+
+from this dude server:
+
+https://www.youtube.com/watch?v=hQAu0YEIF0g
+
 no devStart into package.json
--- src/api.js
+++ src/api.js
@@ -4,7 +4,8 @@ const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 //		this doesnt work:(
-//	app.set('view-engine', 'ejs')
+app.set("views", __dirname + "/views");
+app.set('view engine', 'ejs')
 
 router.get("/", (req, res) => {
 
